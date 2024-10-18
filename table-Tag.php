325e597d97c67a50972a0925c860ed5128c1694e
Hanseeka-Dhingana <hanseeka.bscskdkf22@iba-suk.edu.pk>
2024-10-18 20:29:43 +0500

    <table> Tag

---
+++ table-Tag.php
@@ -0,0 +1,259 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Time Table</title>
+    <link rel="stylesheet" href="table.css">
+
+</head>
+<body>
+    
+ <div class = head>
+  <div > <img src="iba logo.jpg" alt="iba icon"   id = "image">  </div>
+  <div> <h3>Sukkur IBA University-Kandhkot Campus</h3>  </div>
+    
+</div>
+  
+<div >
+     <h1  class = head  style = "margin-top: 0px;">BSCS-V</h1>
+</div>
+
+   <table>
+      <tr >
+         <td class = empty > </td>
+         <td >
+             <h2>1</h2>
+             <h4 > 9:00</h4>
+             <h4> 10:15 AM</h4>
+        </td>
+
+         <td>
+             <p>15 Minutes Break</p>
+             <h4>10:15</h4>
+             <h4>10:30 AM</h4>
+
+         </td>
+
+         <td >
+             <h2>2</h2>
+             <h4 > 10:30</h4>
+             <h4> 11:45 AM</h4>
+        </td>
+
+        <td>
+             <p>15 Minutes Break</p>
+             <h4>11:45 AM</h4>
+             <h4>12:00 PM</h4>
+
+         </td>
+
+        <td >
+             <h2>3</h2>
+             <h4 > 12:00</h4>
+             <h4> 1:15 PM</h4>
+        </td>
+
+        <td>
+             <p>60 Minutes Break</p>
+             <h4>1:45 </h4>
+             <h4>2:15 PM</h4>
+
+         </td>
+
+         <td >
+             <h2>4</h2>
+             <h4 > 2:15</h4>
+             <h4> 3:30 PM</h4>
+         </td>
+
+         <td>
+             <p>15 Minutes Break</p>
+             <h4>3:30 </h4>
+             <h4>3:45 PM</h4>
+
+         </td>
+
+         <td >
+             <h2>5</h2>
+             <h4 > 3:45</h4>
+             <h4> 5:00 PM</h4>
+         </td>
+
+         
+
+      </tr>
+           
+      <tr >
+         <td  > 
+            <h1>Mon</h1>
+         </td>
+         <td >
+            <p>Lab-1</p>
+            <p>Computer Network</p>
+            <p>Faiz Lakhani</p>
+        </td>
+
+         <td rowspan=5 class = break>
+            <h1>15 Minutes Break</h1>
+         </td>
+
+         <td >
+            <p>Room-02</p>
+            <p>Differential Equations</p>
+            <p>Iftikhar Ahmed</p>
+        </td>
+
+        <td rowspan = 5 class = break>
+            <h1>15 Minutes Break</h1>
+        </td>
+        <td >
+            <p>Room-02</p>
+            <p>Probability and Statistics</p>
+            <p>Khair Bux</p>
+        </td>
+
+        <td rowspan = 5 class = break>
+           <h1>60 Minutes Break</h1>
+        </td>
+         <td >
+            <p>Lab-1</p>
+            <p>Computer Network</p>
+            <p>Faiz Lakhani</p>
+         </td>
+
+         <td rowspan = 5 class = break>
+           <h1>15 Minutes Break</h1>
+         </td>
+         <td class = empty> </td>
+
+      </tr>
+                
+      <tr >
+          <td  > 
+            <h1>Tue</h1>
+         </td>
+         <td >
+            <p>Lab-1</p>
+            <p>Computer Network</p>
+            <p>Faiz Lakhani</p>
+        </td>
+
+        <td class = empty> </td>
+
+        <td >
+            <p>Room-01</p>
+            <p>Differential Equations</p>
+            <p>Iftikhar Ahmed</p>
+        </td>
+
+        
+
+         <td >
+             <p>Lab-1</p>
+            <p>Computer Network</p>
+            <p>Faiz Lakhani</p>
+         </td>
+
+        
+         <td >
+            <p>Lab-1</p>
+            <p>Computer Network</p>
+            <p>Faiz Lakhani</p>
+         </td>
+
+         
+
+      </tr>
+
+      <tr >
+         <td  > 
+            <h1>Wed</h1>
+         </td>
+         <td >
+             <h2>1</h2>
+             <h4 > 9:00</h4>
+             <h4> 10:15 AM</h4>
+        </td>
+
+         
+         <td >
+             <h2>2</h2>
+             <h4 > 10:30</h4>
+             <h4> 11:45 AM</h4>
+        </td>
+
+       
+
+        <td >
+             <h2>3</h2>
+             <h4 > 12:00</h4>
+             <h4> 1:15 PM</h4>
+        </td>
+
+        
+
+         <td >
+             <h2>4</h2>
+             <h4 > 2:15</h4>
+             <h4> 3:30 PM</h4>
+         </td>
+
+
+         <td class = empty> </td>
+
+         
+
+      </tr>
+
+      <tr >
+         <td  > 
+            <h1>Thr</h1>
+         </td>
+         <td >
+             <h2>1</h2>
+             <h4 > 9:00</h4>
+             <h4> 10:15 AM</h4>
+        </td>
+
+        <td class = empty> </td>
+
+        <td >
+             <h2>3</h2>
+             <h4 > 12:00</h4>
+             <h4> 1:15 PM</h4>
+        </td>
+
+      
+
+         <td >
+             <h2>4</h2>
+             <h4 > 2:15</h4>
+             <h4> 3:30 PM</h4>
+         </td>
+
+         <td class = empty> </td>
+         
+      </tr>
+
+      <tr>
+        <td  > 
+            <h1>Fri</h1>
+         </td>
+        <td>
+            <p>Lab-1</p>
+            <p>Computer Network</p>
+            <p>Faiz Lakhani</p>
+
+        </td>
+      </tr>
+
+   </table>
+
+   <p>
+    Fall - Semester 2024-w.e.f Monday, 26 August 2024
+   </p>
+    
+    
+</body>
+</html>
