e48ee1a499a5f1906dfe299fdee4ea63bbcafe67
Hanseeka-Dhingana <hanseeka.bscskdkf22@iba-suk.edu.pk>
2024-10-18 20:27:31 +0500

    <P> and <span> tag

---
+++ parragraphAndSpanTag.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <p>
+      This is my first parragraph 
+    </p>
+
+    <p>
+        This is Second parrgraph
+    </p>
+
+    <span>
+        Hello
+    </span>
+
+    <span>
+        world
+    </span>
+
+    <p>Hello <span>world</span></p>
+</body>
+</html>
