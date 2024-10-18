c09a1b185f3fd6cd7f573227131295d60281a7ff
Hanseeka-Dhingana <hanseeka.bscskdkf22@iba-suk.edu.pk>
2024-10-18 20:33:17 +0500

    move from link page to link2 page

---
+++ link.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>This is the anker tag program</title>
+</head>
+<body  style = "background-color: pink;  text-align: center;">
+    
+     <h2 >Click the button to change the page.</h2>
+    
+      <a href="link2.php" target="_blank" rel="noopener noreferrer">New Page</a>
+</body>
+</html>
---
+++ link2.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>This is the new page</title>
+</head>
+<body style = "background-color: lavender;  text-align: center;">
+    
+    <h2>Welcome to New Page of Web</h2>
+    
+     
+      
+</body>
+</html>
